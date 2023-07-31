e1d2d73722ca2e7fa6ff724a990fd339caf4fde0
Maxwell L <mleuthner2002@gmail.com>
2023-07-31 17:34:54 -0500

    now seeds now works

--- schema.sql
+++ schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE department (
 );
 
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,  
@@ -26,13 +26,13 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES roles(id)
     ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
