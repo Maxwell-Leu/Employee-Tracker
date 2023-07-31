51b0cb81b4e4845f3f197d956a6a6655af79afe7
Maxwell L <mleuthner2002@gmail.com>
2023-07-31 13:13:16 -0500

    tables made

--- schema.sql
+++ schema.sql
@@ -3,4 +3,22 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE depatment
+CREATE TABLE depatment (
+    id INT NOT  NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT FOREIGN KEY REFERENCES depatment(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT FOREIGN KEY REFERENCES role(id),
+    manager_id INT FOREIGN KEY REFERENCES employee(id)
+)
