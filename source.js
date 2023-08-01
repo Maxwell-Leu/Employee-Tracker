7a487e97bf89d5cdf694c2a1b8994334792bf93f
Maxwell L <mleuthner2002@gmail.com>
2023-07-31 20:59:14 -0500

    question tree

--- source.js
+++ source.js
@@ -16,7 +16,92 @@ const db = mysql.createConnection(
     },
     console.log(`Conected to the employee_db database.`)
 )
-
+/*
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
-})
+});
+*/
+function init(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            message:'What would you like to do?',
+            name: 'wwyltd',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ]).then((data) => {
+        if (data.wwyltd === 'View all departments'){
+            db.query("SELECT * FROM department;",function (err,results){})
+        }else if(data.wwyltd == 'View all roles'){
+            db.query("SELECT * FROM roles;",function (err,results){})
+        }else if(data.wwyltd == 'View all employees'){
+            db.query("SELECT * FROM employee;",function (err,results){})
+        }else if(data.wwyltd == 'Add a department'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'newDep',
+                    message: 'Enter the new department.'
+                }
+            ]).then((data) => {
+                db.query(`INSERT INTO department (dep_name) VALUES (?);`,data.newDep,function (err,results){
+                    console.table('Department has been added.');
+                })
+            })
+        }else if(data.wwyltd == 'Add a role'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'newRole',
+                    message: 'Enter the new role.'
+                },
+                {
+                    type: 'input',
+                    name: 'depId',
+                    message: 'What is the department id of the role',
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'What is their salary'
+                }
+            ]).then((data) => {
+                db.query(`INSERT INTO roles (title,department_id,salary) VALUES (?,?,CAST(? as DECIMAL));`,[data.newRole,parseInt(data.depId),data.salary],function (err,results){
+                    console.table('Role has been added.');
+                })
+            })
+        }else if(data.wwyltd == 'Add an employee'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'Enter the first name.'
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'Enter the last name',
+                },
+                {
+                    type: 'input',
+                    name: 'roleId',
+                    message: 'Enter is the role id'
+                },
+                {
+                    type: 'input',
+                    name: 'managerId',
+                    message: 'Enter is the id of the manager'
+                }
+            ]).then((data) => {
+                db.query(`INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?);`,[data.firstName,data.lastName,data.roleId,data.managerId],function (err,results){
+                    console.table('employee has been added.');
+                })
+            })
+        }else if(data.wwyltd == 'Update an employee role'){
+            db.query("SELECT * FROM employee;",function (err,results){
+                console.table(results);
+            })
+        }
+    })
+}
+init()
